52c8baba239d4fcc1c05170e1659b309c40342d1
Wesley Arrington <cdsigma271@gmail.com>
2019-09-03 16:11:01 -0700

    Updates

--- src/project/project.jsx
+++ src/project/project.jsx
@@ -6,21 +6,27 @@ export default class Project extends Component {
         return (
             <div className="project">
                     <div className="simple-column">
-                        <h1 className="project-title">{this.props.title}</h1>
+                        
                         <div className="simple-row">
-                            
+                            <div className="project-right-side">
+                                <h1 className="project-title">{this.props.title}</h1>
+                                <div className="project-text-container-div">
+                                    <h3 className="project-description">{this.props.description}</h3>
+                                    <h3 className="project-technologies">{this.props.technologies}</h3>
+                                </div>
+                                <div className="button-container-div">
+                                    <a href={this.props.linkName}>
+                                        <button className="project-link-button"><b>{this.props.linkType}</b></button>
+                                    </a>
+                                </div>
+                            </div>
                             <div className="project-image-div">
                                 <img src={this.props.imageUrl} alt="" className="project-image" />
                             </div>
-                            <div className="project-right-side">
-                                <h3>{this.props.description}</h3>
-                                <h3 className="project-technologies">{this.props.technologies}</h3>
-                            </div>
+                            
                         </div>
                     </div>
-                <a href={this.props.linkName}>
-                    <button className="project-link-button"><b>{this.props.linkType}</b></button>
-                </a>
+                
             </div>
         )
     }
