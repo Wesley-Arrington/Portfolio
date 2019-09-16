43ae3058f30995ece670f8c709f088daf4663433
Wesley Arrington <cdsigma271@gmail.com>
2019-09-16 13:19:56 -0700

    mobile breakpoints

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -9,7 +9,7 @@ export default class About extends Component {
                 <div className="about-color">
                     <div className="about-content-container">
                         <h1 className="about-me-title">ABOUT ME</h1>
-                        <p>
+                        <p className="about-me-para">
                             I'm a designer & front-end developer early in my career. I'm interested in all kinds of visual communication, 
                             but my major focus is on designing and building web, mobile & tablet interfaces. 
                             {/* I also have skills in other fields like blockchain technology specifically Ethereum. */}
--- src/project/project.jsx
+++ src/project/project.jsx
@@ -4,30 +4,49 @@ import './project.css'
 export default class Project extends Component {
     render() {
         return (
+
             <div className="project">
-                    <div className="simple-column">
-                        
-                        <div className="simple-row">
-                            <div className="project-right-side">
-                                <h1 className="project-title">{this.props.title}</h1>
-                                <div className="project-text-container-div">
-                                    <h3 className="project-description">{this.props.description}</h3>
-                                    <h3 className="project-technologies">{this.props.technologies}</h3>
-                                </div>
-                                <div className="button-container-div">
-                                    <a href={this.props.linkName}>
-                                        <button className="project-link-button"><b>{this.props.linkType}</b></button>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="project-image-div">
-                                <img src={this.props.imageUrl} alt="" className="project-image" />
-                            </div>
-                            
+                
+                <div className="desc-image-div">
+                    <div className="project-image-div">
+                        <img src={this.props.imageUrl} alt="" className="project-image" />
+                    </div>
+                    <div className="project-text-container-div">
+                        <h1 className="project-title">{this.props.title}</h1>
+                        <h3 className="project-description">{this.props.description}</h3>
+                        <h3 className="project-technologies">{this.props.technologies}</h3>
+                        <div className="button-container-div">
+                            <a href={this.props.linkName}>
+                                <button className="project-link-button"><b>{this.props.linkType}</b></button>
+                            </a>
                         </div>
                     </div>
-                
+                </div>
             </div>
+            // <div className="project">
+            //         <div className="simple-column">
+                        
+            //             <div className="simple-row">
+            //                 <div className="project-right-side">
+            //                     <h1 className="project-title">{this.props.title}</h1>
+            //                     <div className="project-text-container-div">
+            //                         <h3 className="project-description">{this.props.description}</h3>
+            //                         <h3 className="project-technologies">{this.props.technologies}</h3>
+            //                     </div>
+            //                     <div className="button-container-div">
+            //                         <a href={this.props.linkName}>
+            //                             <button className="project-link-button"><b>{this.props.linkType}</b></button>
+            //                         </a>
+            //                     </div>
+            //                 </div>
+            //                 <div className="project-image-div">
+            //                     <img src={this.props.imageUrl} alt="" className="project-image" />
+            //                 </div>
+                            
+            //             </div>
+            //         </div>
+                
+            // </div>
         )
     }
 }
